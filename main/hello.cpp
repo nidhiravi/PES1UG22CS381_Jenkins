502199262772e0b8f3696076168c16114df3cd15
nidhiravi <142886869+nidhiravi@users.noreply.github.com>
2025-03-18 16:17:35 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "PES1UG22CS381 has successfully built and run this  \n";
+  cout << "PES1UG22CS381 has successfully built and run this Jenkins  \n";
   return 0;
 }

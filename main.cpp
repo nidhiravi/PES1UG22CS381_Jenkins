648c6704c17e9c18bf70450ad4ff33c97cb3c594
nidhiravi <142886869+nidhiravi@users.noreply.github.com>
2025-03-18 16:23:28 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "PES1UG22CS381 has successfully built and run this Jenkins  \n";
+  return 0;
+}
